cc7510882826532768ac1b917f8263752e2a29ad
clgleroux <clement@marketparts.com>
2024-04-04 09:23:53 +0200

    :wrench: fix migrate stores beers

--- db/migrate/20240404071039_create_stores_beers_join_table.rb
+++ db/migrate/20240404071039_create_stores_beers_join_table.rb
@@ -4,9 +4,9 @@ class CreateStoresBeersJoinTable < ActiveRecord::Migration[7.1]
     create_join_table :stores, :beers
 
     # If you want to add an index for faster querying through this join:
-    create_join_table :stores, :beers do |t|
-      t.index :store_id
-      t.index :beer_id
-    end
+    # create_join_table :stores, :beers do |t|
+    #   t.index :store_id
+    #   t.index :beer_id
+    # end
   end
 end
