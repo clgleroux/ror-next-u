fb0751a4bad9f4c18040a79da5e698e5f8bbc1bd
clgleroux <clement@marketparts.com>
2024-04-09 16:49:39 +0200

    :wrench: annotate director

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :integer          not null, primary key
+#  firstName  :string
+#  lastName   :string
+#  store_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
   has_one :store
 end
